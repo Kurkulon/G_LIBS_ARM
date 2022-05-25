d563c2242548a5d52db4938387943594af101f97
Fantex <fantex@hotbox.ru>
2022-05-25 19:10:37 +0400

    +

--- cpp/usic.cpp
+++ cpp/usic.cpp
@@ -1,13 +1,116 @@
 #include "usic.h"
 
-u32 	USIC::_busy_mask = 0;
-u32 	USIC::_alloc_mask = 0;
+u32 		USIC::_busy_mask = 0;
+u32 		USIC::_alloc_mask = 0;
+u32 		USIC::_req_reset_mask = 0;
 
-USIC*	USIC::_usic[USIC_NUM] = {0};
+USIC*		USIC::_usic[USIC_NUM] = {0};
+
+#ifdef CPU_SAME53
+
+const byte	USIC::_usic_pid[USIC_NUM]		= {	PID_SERCOM0,		PID_SERCOM1,		PID_SERCOM2,		PID_SERCOM3,		PID_SERCOM4,		PID_SERCOM5,		PID_SERCOM6,		PID_SERCOM7 		};
+USICHWT*	const USIC::_usic_hw[USIC_NUM]	= { HW::I2C0,			HW::I2C1,			HW::I2C2,			HW::I2C3,			HW::I2C4,			HW::I2C5,			HW::I2C6,			HW::I2C7			};
+const byte	USIC::_usic_gclk[USIC_NUM]		= {	GCLK_SERCOM0_CORE,	GCLK_SERCOM1_CORE,	GCLK_SERCOM2_CORE,	GCLK_SERCOM3_CORE,	GCLK_SERCOM4_CORE,	GCLK_SERCOM5_CORE,	GCLK_SERCOM6_CORE,	GCLK_SERCOM7_CORE	};
+
+#elif defined(CPU_XMC48)
+
+const byte	USIC::_usic_pid[USIC_NUM]		= {	PID_USIC0,		PID_USIC0,		PID_USIC1,		PID_USIC1,		PID_USIC2,		PID_USIC2		};
+USICHWT*	const USIC::_usic_hw[USIC_NUM]	= {	HW::USIC0_CH0,	HW::USIC0_CH1,	HW::USIC1_CH0,	HW::USIC1_CH1,	HW::USIC2_CH0,	HW::USIC2_CH1	};
+
+#elif defined(WIN32)
+
+const u32	USIC::_usic_pid[USIC_NUM] = {PID_USIC0, PID_USIC0, PID_USIC1, PID_USIC1, PID_USIC2, PID_USIC2 };
+
+USICHWT*	const USIC::_uhw[USIC_NUM] = { HW_USIC0, HW_USIC1, HW_USIC2, HW_USIC3, HW_USIC4, HW_USIC5 };
+
+#endif 
 
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+bool USIC::Usic_Connect(byte num)
+{
+	u32 mask = (1UL<<num);
+
+	if (num >= USIC_NUM || (_alloc_mask & mask) || _usic[num] != 0) return false;
+
+	_usic_mask = mask;
+	_alloc_mask |= _usic_mask;
+
+	_usic_num = num;
+
+	_usic[_usic_num] = this;
+	_upid = _usic_pid[_usic_num];
+	_uhw = _usic_hw[_usic_num];
+
+#ifdef CPU_SAME53
+
+	_ugclk = _usic_gclk[_usic_num];
+
+#endif
+
+	return true;
+}
+
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#ifdef CPU_SAME53
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+void USIC::Usic_Update()
+{
+	if ((_req_reset_mask & _usic_mask) && !(_busy_mask & _usic_mask))
+	{
+		HW::MCLK->ClockEnable(_upid);
+
+		_uhw->CTRLA = I2C_SWRST;
+
+		while (_uhw->SYNCBUSY & I2C_SWRST);
+
+		InitHW();
+
+		_req_reset_mask &= ~_usic_mask;
+	};
+}
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#elif defined(CPU_XMC48)
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+void USIC::Usic_Update()
+{
+	if (_req_reset_mask & _usic_mask)
+	{
+		byte num = _usic_num ^ 1;
+
+		u32 mask = (1UL<<num);
+
+		_req_reset_mask |= mask;
+
+		if (!(_busy_mask & (mask|_usic_mask)))
+		{
+			HW::Peripheral_Disable(_upid);
+
+			InitHW();
+
+			if ((_alloc_mask & mask) && _usic[num] != 0)
+			{
+				_usic[num]->InitHW();
+
+			};
+			
+			_req_reset_mask &= ~(mask|_usic_mask);
+		};
+	};
+}
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#endif
+
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
--- cpp/usic.h
+++ cpp/usic.h
@@ -13,11 +13,15 @@
 
 #ifdef CPU_SAME53
 
-#define USIC_NUM 6
+#define USIC_NUM 8
+
+typedef T_HW::S_I2C0 USICHWT;
 
 #elif defined(CPU_XMC48)
 
-#define USIC_NUM 8
+#define USIC_NUM 6
+
+typedef T_HW::USIC_CH_Type USICHWT;
 
 #elif defined(WIN32)
 
@@ -27,18 +31,40 @@
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-class USIC
+class USIC // Universal Serial Interface Channel 
 {
 	protected:
 
-		static 	u32 	_busy_mask;
-		static 	u32 	_alloc_mask;
+		static 	u32 		_busy_mask;
+		static 	u32 		_alloc_mask;
+		static 	u32 		_req_reset_mask;
 
-		static	USIC*	_usic[USIC_NUM];
+		static  const byte	_usic_pid[USIC_NUM];
+		static	USIC*		_usic[USIC_NUM];
+
+		static	USICHWT*	const _usic_hw[USIC_NUM];
+
+#ifdef CPU_SAME53
+
+		static  const byte	_usic_gclk[USIC_NUM];
+
+		byte				_ugclk;
+
+#endif
+		byte				_usic_num;
+		u32					_usic_mask;
+
+		u32					_upid;
+		USICHWT*			_uhw;
 
 	public:
 
-		USIC() {}
+		USIC() : _usic_num(~0), _usic_mask(0) {}
+
+		bool Usic_Connect(byte num);
+		void Usic_Update();
+
+		void RequestReset() { _req_reset_mask |= _usic_mask; }
 
 		virtual void InitHW() {}
 };
